06c64b396251364793df96e04f85d2a2a8712fab
gibbssonfarias30 <gibbfarc17@gmail.com>
2023-03-28 18:06:54 -0500

    unit testing lifecycle

--- src/test/java/com/backfcdev/reviewjunit/calculator/CalculatorTest.java
+++ src/test/java/com/backfcdev/reviewjunit/calculator/CalculatorTest.java
@@ -1,12 +1,34 @@
 package com.backfcdev.reviewjunit.calculator;
 
 import com.backfcdev.reviewjunit.Calculator;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 public class CalculatorTest {
+
+    Calculator calculator;
+    //unit testing lifecycle
+
+    @BeforeAll
+    public static void first(){
+        System.out.println("runs at the beginning of all unit tests, one time only");
+    }
+
+    @AfterAll
+    public static void last(){
+        System.out.println("is executed at the end of all unit tests, for one time");
+    }
+
+    @BeforeEach
+    public void objectInstance(){
+        calculator = new Calculator();
+        System.out.println("is executed at the beginning of each unit test");
+    }
+
+    @AfterEach
+    public void afterTheTest(){
+        System.out.println("runs at the end of each unit test");
+    }
 
     @Test
     @DisplayName("Test Sum")
@@ -19,16 +41,16 @@ public class CalculatorTest {
     @Test
     @DisplayName("Test Divide")
     void divideTest(){
-        Calculator calculator = new Calculator();
+        calculator = new Calculator();
         assertTrue(calculator.divide(10, 2) == 5);
         assertFalse(calculator.divide(6, 1) == 12);
     }
 
     @Test
     @DisplayName("Test Multiplication")
-    @Disabled
+    //@Disabled
     void multiplicationTest(){
-        Calculator calculator = new Calculator();
+        calculator = new Calculator();
         assertTrue(calculator.multiplication(10, 2) == 5);
     }
 }
